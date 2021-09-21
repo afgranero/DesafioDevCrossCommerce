dbae94b8a9ef37f9b78bc4d7cb56fbc638685bba
Airton da Fonseca Granero <agranero@gmail.com>
2021-09-21 02:00:58 -0300

    Finished extraction step.

--- config.py
+++ config.py
@@ -5,14 +5,21 @@ EXTRACT_PARAM = 'page'
 
 RETRY_MAX_COUNT = 5
 RETRY_INTERVAL = 5
+RETRY_LONG_INTERVAL = 50
 
-LOG_FILE_TEMPLATE = './logs/totem-{}.log'
-LOG_ID = 'DesafioDevCrossCommerce'
+TASK_COUNT = 101
+
+LOG_FILE_TEMPLATE = "./logs/totem-{}.log"
+LOG_ID = "DesafioDevCrossCommerce"
 
 
-MESSAGE_REQUEST_INIT ="Class initialized."
-MESSAGE_REQUEST_SUCCESS = "Request suceess for page '{}'."
+MESSAGE_REQUEST_INIT = "Class initialized."
+MESSAGE_REQUEST_SUCCESS = "Request success for page '{}'."
 MESSAGE_REQUEST_ERROR = "Request error on retry '{}' of page '{}': '{}' - '{}'."
 MESSAGE_REQUEST_MAX_RETRIES = "Request max retries of '{}' reached."
 MESSAGE_REQUEST_LAST_PAGE = "Last page '{}' reached."
+MESSAGE_REQUESTS_PARALLEL_ROUND = "Beginning a new round of '{}' parallel requests for pages from '{}' to '{}'."
+MESSAGE_REQUESTS_PARALLEL_ERROR = "Error '{}' on round of parallel requests for pages from '{}' to '{}', retry '{}'."
+MESSAGAGE_SLEEPING ="Sleeeping for '{}' seconds."
+MESSAGE_SUCCESS = "Success, all pages scanned: '{}' elements fetched."
 
--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 import time
 
 import requests
+import asyncio
+import aiohttp
 
 from config import *
 from log import Log
@@ -11,36 +13,72 @@ class Extract:
     def __init__(self, url):
         self.logger = Log.init_logger(LOG_ID, LOG_FILE_TEMPLATE)
         self.url = url
+        self.out_list = []
+        self.loop = asyncio.get_event_loop()
         self.logger.info(MESSAGE_REQUEST_INIT)
 
-    def get_list(self):
-        page = 1
-        out_list = []
+    async def get_pages(self, page_start, page_stop):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for page in range(page_start, page_stop):
+                tasks.append(self.get_page(session=session, page=page))
+
+            in_list = await asyncio.gather(*tasks, return_exceptions=False)
+            return in_list
+
+    async def get_page(self, session, page):
         retry_count = 0
-
         while True:
-            response = requests.get(self.url, data = {EXTRACT_PARAM: f'{page}'})
-            if  response.status_code == requests.status_codes.codes.OK:
-                self.logger.info(MESSAGE_REQUEST_SUCCESS.format(page))
-                in_list = response.json()
-                if len(in_list) == 0:
-                    self.logger.info(MESSAGE_REQUEST_LAST_PAGE.format(page))
-                    break
+            params = f'?{EXTRACT_PARAM}={page}'
+            async with session.get(self.url + params) as response:
+                if response.status == requests.status_codes.codes.OK:
+                    self.logger.info(MESSAGE_REQUEST_SUCCESS.format(page))
+                    return await response.json()
+                else:
+                    self.logger.error(MESSAGE_REQUEST_ERROR.format(retry_count, page, response.status, response.reason))
 
-                out_list.extend(in_list)
+                    # the first retry is immediate
+                    if retry_count > 1:
+                        time.sleep(RETRY_INTERVAL)
+
+                    retry_count += 1
+                    if retry_count >= RETRY_MAX_COUNT:
+                        self.logger.error(MESSAGE_REQUEST_MAX_RETRIES.format(retry_count))
+                        # TODO permanent failure raise exception
+
+    def run(self):
+        page_start = 1
+        page_stop = TASK_COUNT
+        retry_count = 0
+        while True:
+            self.logger.info(MESSAGE_REQUESTS_PARALLEL_ROUND.format(TASK_COUNT, page_start, page_stop))
+
+            try:
+                result = asyncio.run(self.get_pages(page_start, page_stop))
                 retry_count = 0
-                page += 1
-            else:
-                self.logger.error(MESSAGE_REQUEST_ERROR.format(retry_count, page, response.status_code, response.reason))
-                time.sleep(RETRY_INTERVAL)
+            except Exception as e:
+                self.logger.error(MESSAGE_REQUESTS_PARALLEL_ERROR.format(str(e), page_start, page_stop, retry_count))
                 retry_count += 1
-
                 if retry_count >= RETRY_MAX_COUNT:
                     self.logger.error(MESSAGE_REQUEST_MAX_RETRIES.format(retry_count))
+                    break
 
-        return out_list
+                self.logger.info(MESSAGAGE_SLEEPING.format(RETRY_LONG_INTERVAL))
+                time.sleep(RETRY_LONG_INTERVAL)
+                continue
+
+            result_lists = [e['numbers'] for e in result]
+            self.out_list.extend(sum(result_lists, []))
+            if [] in result_lists:
+                break
+
+            page_start = page_stop
+            page_stop = page_start + TASK_COUNT - 1
+
+        self.logger.info(MESSAGE_SUCCESS.format(len(self.out_list)))
+        return self.out_list
 
 
 if __name__ == '__main__':
     extract = Extract(EXTRACT_ADDRESS)
-    extract.get_list()
+    extract.run()
